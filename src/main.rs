f8eb60e65e434479d49e6996c53a6da0b9f54a69
Raúl Ferràs <raul.ferras@lifullconnect.com>
2023-09-09 13:25:41 +0200

    Implement main emulator loop.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
+use std::fs;
+use macroquad::prelude::next_frame;
 use crate::chip8::Chip8;
 
 mod chip8;
 
-fn main() {
-    let emulator: Chip8 = Chip8::new();
-    println!("Hello, world!");
+#[macroquad::main("Rusted Chip8")]
+async fn main() {
+    let mut emulator: Chip8 = Chip8::new();
+
+    let result = fs::read("roms/Particle Demo [zeroZshadow, 2008].ch8");
+    //let result = fs::read("roms/Space Invaders [David Winter].ch8");
+    let program: Vec<u8>;
+    match result {
+        Ok(contents) => {
+            program = contents.into();
+        },
+        Err(e) => {
+            panic!("Could not open rom");
+        }
+    }
+    println!("Rom opened: len {}", program.len());
+    emulator.load(program);
+    emulator.start();
+    loop {
+        emulator.run();
+        next_frame().await;
+    }
 }
